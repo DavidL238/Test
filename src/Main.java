90378670d35fa7081ae97c188ef65ff4306c3184
BT_1E10_06 <dliu3541@bths.edu>
2022-02-04 11:26:28 -0500

    B)

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<String> a = new ArrayList<String>();
         a.add("a");
+        a.add("b");
         System.out.println(a);
     }
 }
